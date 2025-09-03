c430d7007ea163dd6be069fafc7ae404dd3c0170
rlozl15 <eunjikim.dev@gmail.com>
2025-09-03 22:29:08 +0900

    feat: 포인트 충전 기능 및 단위 테스트 구현

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,43 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    private static final long MAXIMUM_POINT = 1_000_000L;
+
+    // 예외 메시지
+    private static final String ERROR_INVALID_USER_ID = "잘못된 사용자 ID입니다.";
+    private static final String ERROR_INVALID_AMOUNT = "충전 포인트는 1원 이상이어야 합니다.";
+    private static final String ERROR_EXCEED_MAXIMUM_POINT = "최대 보유 가능한 포인트는 100만 포인트입니다.";
+
+
+    public UserPoint charge(long userId, long amount) {
+        if (userId < 1) {
+            throw new IllegalArgumentException(ERROR_INVALID_USER_ID);
+        }
+
+        if (amount < 1) {
+            throw new IllegalArgumentException(ERROR_INVALID_AMOUNT);
+        }
+
+        UserPoint storedUserPoint = userPointTable.selectById(userId);
+        long newAmount = storedUserPoint.point() + amount;
+
+        if (newAmount > MAXIMUM_POINT) {
+            throw new IllegalArgumentException(ERROR_EXCEED_MAXIMUM_POINT);
+        }
+
+        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, newAmount);
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updatedUserPoint;
+    }
+}
---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,153 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PointServiceTest {
+    @Mock
+    private PointHistoryTable pointHistoryTable;
+    @Mock
+    private UserPointTable userPointTable;
+    @InjectMocks
+    private PointService pointService;
+
+    @Nested
+    @DisplayName("포인트 충전 테스트")
+    public class ChargePointTest {
+        /*
+         [작성이유]
+         기존 사용자가 포인트를 충전할 경우, 정상적으로 포인트가 업데이트 되는지 확인하기 위해 작성함
+         경계값인 보유 10만 포인트 충전 90만 포인트로 충전해봄 (최대 보유 가능한 포인트가 100만)
+         */
+        @Test
+        void 기존_사용자가_적정_포인트를_충전할_때_포인트가_정상적으로_누적된다() {
+            // given
+            long userId = 1L;
+            long existingPoint = 100_000L;
+            long chargeAmount = 900_000L;
+            long newAmount = existingPoint + chargeAmount;
+
+            UserPoint existingUserPoint = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+            UserPoint updatedUserPoint = new UserPoint(userId, newAmount, System.currentTimeMillis());
+
+            when(userPointTable.selectById(userId)).thenReturn(existingUserPoint);
+            when(userPointTable.insertOrUpdate(userId, newAmount)).thenReturn(updatedUserPoint);
+
+            // when
+            UserPoint result = pointService.charge(userId, chargeAmount);
+
+            // then
+            assertThat(result.id()).isEqualTo(userId);
+            assertThat(result.point()).isEqualTo(newAmount);
+        }
+        /*
+         [작성이유]
+         신규 사용자가 포인트를 충전할 경우, 정상적으로 신규 사용자의 포인트가 업데이트 되는지 확인하기 위해 작성함
+         경계값인 100만 포인트로 충전해봄 (최대 보유 가능한 포인트가 100만)
+         */
+        @Test
+        void 신규_사용자가_적정_포인트를_충전할_때_포인트가_정상적으로_누적된다() {
+            // given
+            long userId = 1L;
+            long chargeAmount = 1_000_000L;
+
+            UserPoint existingUserPoint = UserPoint.empty(userId);
+            UserPoint updatedUserPoint = new UserPoint(userId, chargeAmount, System.currentTimeMillis());
+
+            when(userPointTable.selectById(userId)).thenReturn(existingUserPoint);
+            when(userPointTable.insertOrUpdate(userId, chargeAmount)).thenReturn(updatedUserPoint);
+
+            // when
+            UserPoint result = pointService.charge(userId, chargeAmount);
+
+            // then
+            assertThat(result.id()).isEqualTo(userId);
+            assertThat(result.point()).isEqualTo(chargeAmount);
+        }
+        /*
+         [작성이유]
+         사용자가 포인트를 충전할 경우, 충전된 포인트 내역이 정상적으로 기록되는지 확인하기 위해 작성함
+         */
+        @Test
+        void 포인트를_충전할_때_충전_내역이_정상적으로_기록된다() {
+            // given
+            long userId = 1L;
+            long chargeAmount = 5000L;
+
+            UserPoint existingUserPoint = UserPoint.empty(userId);
+
+            when(userPointTable.selectById(userId)).thenReturn(existingUserPoint); // 메소드 내에서 storedUserPoint 사용함으로 값 부여해야함
+
+            // when
+            pointService.charge(userId, chargeAmount);
+
+            // then
+            verify(pointHistoryTable, times(1)).insert(
+                    eq(userId),
+                    eq(chargeAmount),
+                    eq(TransactionType.CHARGE),
+                    anyLong()
+            );
+        }
+        /*
+         [작성이유]
+         사용자 정보가 잘못되었을 경우 (0 이하의 ID), 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 사용자_정보가_잘못되었을_때_충전하면_예외가_발생한다() {
+            // given
+            long invalidUserId = 0L;
+            long chargeAmount = 5000L;
+
+            // when & then
+            assertThatThrownBy(() -> pointService.charge(invalidUserId, chargeAmount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("잘못된 사용자 ID입니다.");
+        }
+        /*
+         [작성이유]
+         사용자가 충전 불가능한 0 이하의 포인트를 충전할 경우, 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 충전할_포인트가_0_이하일_때_충전하면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long invalidChargeAmount  = 0L;
+
+            // when & then
+            assertThatThrownBy(() -> pointService.charge(userId, invalidChargeAmount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("충전 포인트는 1원 이상이어야 합니다.");
+        }
+        /*
+         [작성이유]
+         충전했을 때 누적 포인트가 100만을 넘는 경우, 예외가 발생하는지 확인하기 위해 작성함 (최대 보유 가능한 포인트가 100만)
+         */
+        @Test
+        void 충전_후_누적_포인트가_1_000_000을_넘으면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long chargeAmount  = 1_000_001L;
+
+            UserPoint existingUserPoint = UserPoint.empty(userId);
+
+            when(userPointTable.selectById(userId)).thenReturn(existingUserPoint);
+
+            // when & then
+            assertThatThrownBy(() -> pointService.charge(userId, chargeAmount))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("최대 보유 가능한 포인트는 100만 포인트입니다.");
+        }
+    }
+}
