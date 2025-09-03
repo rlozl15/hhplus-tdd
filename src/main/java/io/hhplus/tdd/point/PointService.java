0c006d42cc84f2056511a5e6bb58e225551dfe62
rlozl15 <eunjikim.dev@gmail.com>
2025-09-04 04:05:52 +0900

    feat: 포인트 사용 기능 및 단위 테스트 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -15,8 +15,11 @@ public class PointService {
 
     // 예외 메시지
     private static final String ERROR_INVALID_USER_ID = "잘못된 사용자 ID입니다.";
-    private static final String ERROR_INVALID_AMOUNT = "충전 포인트는 1원 이상이어야 합니다.";
+    private static final String ERROR_INVALID_CHARGE_AMOUNT = "충전 포인트는 1포인트 이상이어야 합니다.";
     private static final String ERROR_EXCEED_MAXIMUM_POINT = "최대 보유 가능한 포인트는 100만 포인트입니다.";
+    private static final String ERROR_INSUFFICIENT_POINT = "포인트 잔액이 부족합니다.";
+    private static final String ERROR_INVALID_USE_AMOUNT = "사용 포인트는 최소 100포인트 이상이어야 합니다.";
+    private static final String ERROR_MINIMUM_POINT_UNIT = "사용 포인트는 최소 사용 단위인 100의 배수여야 합니다.";
 
     public UserPoint getPoint(long userId) {
         if (userId < 1) {
@@ -27,7 +30,7 @@ public class PointService {
 
     public UserPoint charge(long userId, long amount) {
         if (amount < 1) {
-            throw new IllegalArgumentException(ERROR_INVALID_AMOUNT);
+            throw new IllegalArgumentException(ERROR_INVALID_CHARGE_AMOUNT);
         }
 
         UserPoint storedUserPoint = getPoint(userId);
@@ -39,6 +42,28 @@ public class PointService {
 
         UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, newAmount);
         pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updatedUserPoint;
+    }
+
+    public UserPoint use(long userId, long amount) {
+        if (amount < 100) {
+            throw new IllegalArgumentException(ERROR_INVALID_USE_AMOUNT);
+        }
+
+        if (amount % 100 != 0) {
+            throw new IllegalArgumentException(ERROR_MINIMUM_POINT_UNIT);
+        }
+
+        UserPoint storedUserPoint = getPoint(userId);
+        long newAmount = storedUserPoint.point() - amount;
+
+        if (newAmount < 0) {
+            throw new IllegalArgumentException(ERROR_INSUFFICIENT_POINT);
+        }
+
+        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, newAmount);
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
 
         return updatedUserPoint;
     }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -188,7 +188,7 @@ public class PointServiceTest {
             // when & then
             assertThatThrownBy(() -> pointService.charge(userId, invalidChargeAmount))
                     .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage("충전 포인트는 1원 이상이어야 합니다.");
+                    .hasMessage("충전 포인트는 1포인트 이상이어야 합니다.");
         }
         /*
          [작성이유]
@@ -208,6 +208,150 @@ public class PointServiceTest {
             assertThatThrownBy(() -> pointService.charge(userId, chargeAmount))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("최대 보유 가능한 포인트는 100만 포인트입니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("포인트 사용 테스트")
+    public class UsePointTest {
+        /*
+         [작성이유]
+         기존 사용자가 현재 보유하고 있는 포인트 이내를 사용할 경우, 정상적으로 포인트가 사용되는지 확인하기 위해 작성함
+         */
+        @Test
+        void 기존_사용자가_보유_포인트보다_적은_포인트를_사용할_때_포인트가_정상적으로_사용된다() {
+            // given
+            long userId = 1L;
+            long existingPoint = 100_000L;
+            long usePoint = 10_000L;
+            long remainPoint = 90_000L;
+
+            UserPoint userPoint = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+            UserPoint updatedUserPoint = new UserPoint(userId, remainPoint, System.currentTimeMillis());
+
+            when(userPointTable.selectById(userId)).thenReturn(userPoint);
+            when(userPointTable.insertOrUpdate(userId, remainPoint)).thenReturn(updatedUserPoint);
+
+            // when
+            UserPoint result = pointService.use(userId, usePoint);
+
+            // then
+            assertThat(result.id()).isEqualTo(userId);
+            assertThat(result.point()).isEqualTo(remainPoint);
+        }
+        /*
+         [작성이유]
+         기존 사용자가 현재 보유하고 있는 모든 포인트를 사용할 경우, 정상적으로 포인트가 사용되는지 확인하기 위해 작성함
+         */
+        @Test
+        void 기존_사용자가_보유_포인트_전부를_사용할_때_포인트가_정상적으로_사용된다() {
+            // given
+            long userId = 1L;
+            long existingPoint = 100_000L;
+            long usePoint = 100_000L;
+            long remainPoint = 0L;
+
+            UserPoint userPoint = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+            UserPoint updatedUserPoint = new UserPoint(userId, 0, System.currentTimeMillis());
+
+            when(userPointTable.selectById(userId)).thenReturn(userPoint);
+            when(userPointTable.insertOrUpdate(userId, 0)).thenReturn(updatedUserPoint);
+
+            // when
+            UserPoint result = pointService.use(userId, usePoint);
+
+            // then
+            assertThat(result.id()).isEqualTo(userId);
+            assertThat(result.point()).isEqualTo(remainPoint);
+        }
+        /*
+         [작성이유]
+         포인트를 사용할 경우, 사용 내역이 정상적으로 기록되는지 확인하기 위해 작성함
+         */
+        @Test
+        void 포인트를_사용할_때_사용_내역이_정상적으로_기록된다() {
+            // given
+            long userId = 1L;
+            long existingPoint = 100_000L;
+            long usePoint = 50_000L;
+            UserPoint userPoint = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+            when(userPointTable.selectById(userId)).thenReturn(userPoint);
+
+            // when
+            pointService.use(userId, usePoint);
+
+            // then
+            verify(pointHistoryTable, times(1)).insert(
+                    eq(userId),
+                    eq(usePoint),
+                    eq(TransactionType.USE),
+                    anyLong()
+            );
+        }
+        /*
+         [작성이유]
+         기존 사용자가 현재 보유하고 있는 포인트보다 많은 포인트를 사용할 경우, 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 기존_사용자가_잔액이_부족할_때_포인트_사용을_시도하면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long existingPoint = 100_000L;
+            long usePoint = 100_100L;
+            UserPoint userPoint = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+            when(userPointTable.selectById(userId)).thenReturn(userPoint);
+
+            // when & then
+            assertThatThrownBy(() -> pointService.use(userId, usePoint))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("포인트 잔액이 부족합니다.");
+        }
+        /*
+         [작성이유]
+         초기 0 포인트가 주어지는 신규 사용자가 포인트 사용을 시도하는 경우, 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 신규_사용자가_포인트_사용을_시도하면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long usePoint = 100L;
+            UserPoint userPoint = UserPoint.empty(userId);
+            when(userPointTable.selectById(userId)).thenReturn(userPoint);
+
+            // when & then
+            assertThatThrownBy(() -> pointService.use(userId, usePoint))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("포인트 잔액이 부족합니다.");
+        }
+        /*
+         [작성이유]
+         최소로 요청 가능한 사용 포인트가 100일 때 그보다 적은 포인트를 사용하는 경우, 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 사용_요청_포인트가_최소_사용_단위인_100보다_작으면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long usePoint = 99L;
+
+            // when & then
+            assertThatThrownBy(() -> pointService.use(userId, usePoint))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("사용 포인트는 최소 100포인트 이상이어야 합니다.");
+        }
+        /*
+         [작성이유]
+         사용 포인트가 최소 사용 단위인 100의 배수가 아닐 경우, 예외가 발생하는지 확인하기 위해 작성함
+         */
+        @Test
+        void 사용_요청_포인트가_최소_사용_단위의_배수가_아니면_예외가_발생한다() {
+            // given
+            long userId = 1L;
+            long usePoint = 999L;
+
+            // when & then
+            assertThatThrownBy(() -> pointService.use(userId, usePoint))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("사용 포인트는 최소 사용 단위인 100의 배수여야 합니다.");
         }
     }
 }
